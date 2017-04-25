98508533697c8fb8e9898d0f1788be63a301f760
Yaqi Miao <yaqimiao@berkeley.edu>
2017-04-25 00:58:12 -0700

    done with milestone report

---
+++ code/build/CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/osdfont.c" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/osdfont.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "../CGL/deps/glew/include"
+  "/usr/local/include/freetype2"
+  "../CGL/include/CGL"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/base64.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/base64.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/color.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/color.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/complex.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/complex.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/lodepng.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/lodepng.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/matrix3x3.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/matrix3x3.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/matrix4x4.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/matrix4x4.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/osdtext.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/osdtext.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/quaternion.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/quaternion.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/spectrum.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/spectrum.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/tinyxml2.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/tinyxml2.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/vector2D.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/vector2D.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/vector3D.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/vector3D.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/vector4D.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/vector4D.cpp.o"
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/CGL/src/viewer.cpp" "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/src/CMakeFiles/CGL.dir/viewer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "../CGL/deps/glew/include"
+  "/usr/local/include/freetype2"
+  "../CGL/include/CGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/yaqi/Academic/CS/184/hw/fluid-simulation/code/build/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- docs/milestone.md
+++ docs/milestone.md
@@ -4,17 +4,38 @@
 We decided to do the position based simulation of the fluid motion first. We implemented a basic UI that can be controlled with:
 
 Key | Description
+--- | ---
 ESC, Q | Close window
 up, down, left, right | Rotate view
 Space | Run simulation for 1 step
 
 Now we can run a simple simulation of fluid with the effect of gravity.
 
+![alt text](images/milestone.gif)
 
 ## What we have accomplished
+We implemented the simulation of fluid dynamics with particles. Within our simulation loop, we have done:
+1. Update the new falling position, assign position to last_position and store the new position after delta_t time elapsed.
+2. Test neighboring particles with hash and map to see if particles fall onto each other and correct their positions if this happens.
+3. Collision with the bounding box (water tank). We don't allow the particles, of course, fall out of the bounding box. 
 
 ## What need to be fixed
+1. (Most priority) In step 2 where we test if neighboring particles fall onto each other, the plan after that was to recalculate the force exerted on each particle and update their velocity with regard to this new acceleration. This still doesn't work yet.
+2. (Most priority) In collision of particles with other static object such as plane, now what we did is just to stop it before it collide. What really needs to be done is to calculate the force on the particle when it collide with other object. We expect to see the particles bounce back and forward in the water tank.
+3. (Less priority) Although we are able to roate the view through direction keys, rotate on the y direction still need to be fixed.
+4. (Less priority) Consider moving current progress to another code base adapted from clothsim.
 
-## What need to be added
+## What may need to be added
+1. Adding vorticity and viscosity to our simulation. This may require adding new constraints to the particles.
+2. Collision with other objects, such as sphere or other irregular meshes.
+3. More UI features.
+4. Running on GPU.
+5. Integration with raytracer.
+
+## Reference
+1. James Jia, Michelle Nguyen, Angela Hsueh _Final Project: Fluid Simulation_ http://jamesjia.com/cs184-fluidsim
 
 ## Updated work plan
+This week (4/25~4/28) must fix all the most priority bugs.
+Next week (4/29-5/5) try to do as much as we can in the items that needs to be added.
+
